70302170590bbd70c4a3a82a44760a4c254c3416
Akash Bhardwaj <akashbdj@gmail.com>
2015-03-20 14:14:08 +0530

    Wired collection into  template.

--- server/fixtures.js
+++ server/fixtures.js
@@ -3,10 +3,12 @@ if (Posts.find().count() === 0) {
     title: 'Introducing Telescope',
     url: 'http://sachagreif.com/introducing-telescope/'
   });
+
   Posts.insert({
     title: 'Meteor',
     url: 'http://meteor.com'
   });
+
   Posts.insert({
     title: 'The Meteor Book',
     url: 'http://themeteorbook.com'
